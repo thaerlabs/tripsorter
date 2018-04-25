8057f1ba509bed009a6850e56f0a928d14560a62
thaerlabs <abbas.t249@gmail.com>
2018-04-25 17:58:44 +0300

    project init

---
+++ webpack.config.babel.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+const parts = require('./webpack.parts');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const commonConfig = merge(
+  {
+    entry: {
+      app: './src/index.tsx'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.join(__dirname, 'build'),
+      publicPath: '/'
+    },
+    resolve: {
+      extensions: ['.js'],
+      modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'async',
+        minSize: 30000,
+        minChunks: 1,
+        maxAsyncRequests: 5,
+        maxInitialRequests: 3,
+        automaticNameDelimiter: '~',
+        name: true,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      }
+    }
+  },
+  parts.typescript,
+  parts.tslint,
+  parts.html,
+  parts.css,
+  parts.images,
+  parts.fonts,
+  parts.json
+);
+
+const getConfig = TARGET => {
+  switch (TARGET) {
+    case 'build':
+      return merge(commonConfig, {
+        mode: 'production'
+      });
+    case 'dev':
+    default:
+      return merge(
+        commonConfig,
+        {
+          mode: 'development',
+          devtool: 'cheap-module-eval-source-map'
+        },
+        parts.devServer
+      );
+  }
+};
+
+module.exports = getConfig(TARGET);
---
+++ webpack.parts.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports.typescript = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx']
+  }
+};
+
+module.exports.tslint = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        enforce: 'pre',
+        loader: 'tslint-loader'
+      }
+    ]
+  }
+};
+
+module.exports.css = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
+
+module.exports.html = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
+
+module.exports.images = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
+
+module.exports.fonts = {
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
+
+module.exports.json = {
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.json', '.js']
+  }
+};
+
+module.exports.devServer = {
+  devServer: {
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    port: process.env.PORT || 3000,
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'public'),
+    hot: true,
+    compress: true,
+    overlay: {
+      errors: true,
+      warnings: false
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+};
+
+module.exports.setEnvVar = (name, value) => {
+  return {
+    plugins: [
+      new webpack.DefinePlugin({
+        [name]: JSON.stringify(value)
+      })
+    ]
+  };
+};
